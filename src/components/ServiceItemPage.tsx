80e1ba1fc60cf7ceed31c8ef099557aaab732c46
hoach-linux <hoachprogrammer@gmail.com>
2023-09-16 18:31:41 +0700

    added skeleton for all service item page components

--- src/components/ServiceItemPage.tsx
+++ src/components/ServiceItemPage.tsx
@@ -44,21 +44,42 @@ export default function ServicesItemPage() {
                     {service.image ? (
                         <Image src={`${url}/assets/${service.image}`} width={405} height={405} alt="image" className="mt-10 lg:mt-0 lg:ml-10 rounded-lg object-cover min-w-full lg:min-w-0" />
                     )
-                        : <Skeleton variant="rounded" className="lg:ml-10 min-h-full mt-10 lg:mt-0">
+                        : <Skeleton variant="rounded" className="lg:!ml-10 !min-h-full !mt-10 lg:!mt-0">
                             <Image src={`${url}/assets/${service.image}`} width={405} height={405} alt="image" className="mt-10 lg:mt-0 lg:ml-10 rounded-lg object-cover min-w-full lg:min-w-0" />
                         </Skeleton>
                     }
                     <div className="mt-10 flex flex-col lg:mt-0 lg:ml-10">
-                        <h1 className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">{service.title}</h1>
-                        <h1 className="mt-3 text-4xl font-bold text-gray-500 sm:text-3xl sm:tracking-tight lg:text-3xl">
-                            ${service.price}
-                        </h1>
+                        {service.title ? (
+                            <h1 className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">{service.title}</h1>
+
+                        ) :
+                            <Skeleton variant="rounded" className="!min-w-full">
+                                <h1 className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">service title</h1>
+                            </Skeleton>
+                        }
+                        {service.price ? (
+                            <h1 className="mt-3 text-4xl font-bold text-gray-500 sm:text-3xl sm:tracking-tight lg:text-3xl">
+                                ${service.price}
+                            </h1>
+                        ) : <Skeleton variant="rounded" className="!mt-3">
+                            <h1 className="mt-3 text-4xl font-bold text-gray-500 sm:text-3xl sm:tracking-tight lg:text-3xl">
+                                service price
+                            </h1>
+                        </Skeleton>}
                         <div className="mt-10 mb-5 border-t border-gray-200 pt-10 font-bold">
                             Miêu tả
                         </div>
-                        <p className="max-w-xl mb-5">
-                            {service.description}
-                        </p>
+                        {service.description ?
+                            <p className="max-w-xl mb-5">
+                                {service.description}
+                            </p>
+                            :
+                            <Skeleton variant="rounded" className="!max-w-xl !mb-5">
+                                <p className="max-w-xl mb-5">
+                                    service description
+                                </p>
+                            </Skeleton>
+                        }
                         <Button onClick={() => {
                             setServicesOrder(service)
                             router.push("/order")
