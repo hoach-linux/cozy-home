bed6a976a052968090e68e6419e19dcebddfa19c
hoach-linux <hoachprogrammer@gmail.com>
2023-09-16 12:08:07 +0700

    fixed bug with tailwindcsss

--- src/components/OrderPage.tsx
+++ src/components/OrderPage.tsx
@@ -16,7 +16,7 @@ export default function OrderPage() {
 
     return (
         <section className="mx-auto max-w-lg px-4 sm:px-6 lg:px-8 min-h-screen flex items-center justify-center flex-col">
-            <button onClick={() => router.back()} className="w-48 hover:w-full hover:px-3 hover:py-3 mb-1 transition-all ease-in duration-500 flex items-center justify-center rounded-md border border-transparent py-2 text-base font-medium text-orange-600 shadow-sm bg-orange-100 sm:px-2"><ArrowBackIosNewRounded /></button>
+            <button onClick={() => router.back()} className="w-48 hover:w-full hover:px-3 hover:py-3 mb-1 transition-all ease-in-out duration-200 flex items-center justify-center rounded-md border border-transparent py-2 text-base font-medium text-orange-600 shadow-sm bg-orange-100 sm:px-2"><ArrowBackIosNewRounded /></button>
             <TextField label="Họ, tên" type="text" fullWidth margin="dense" color="warning" onChange={(event) => setOrderInfo({ ...orderInfo, name: event.target.value })} />
             <TextField label="Địa chỉ" type="text" fullWidth margin="dense" color="warning" onChange={(event) => setOrderInfo({ ...orderInfo, address: event.target.value })} />
             <TextField label="E-mail" type="email" fullWidth margin="dense" color="warning" onChange={(event) => setOrderInfo({ ...orderInfo, email: event.target.value })} />
