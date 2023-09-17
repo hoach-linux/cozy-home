9771f8203bc05103b30afe0a84cbfb5d7f14ac91
hoach-linux <hoachprogrammer@gmail.com>
2023-09-17 10:25:16 +0700

    redesigned

--- src/app/services/[name]/page.tsx
+++ src/app/services/[name]/page.tsx
@@ -1,11 +1,16 @@
 import { AnimationLayout } from "@/layouts/AnimationLayout";
 import ServicesItemPage from "@/components/ServiceItemPage";
+import Link from "next/link";
+import { ArrowBackIosNewRounded } from "@mui/icons-material";
 
 export default function ServiceItem() {
 
     return (
-        <AnimationLayout>
-            <ServicesItemPage />
-        </AnimationLayout>
+        <div>
+            <Link href={"/"} className="z-10 fixed top-0 left-0 min-w-full hover:p-3 flex items-center justify-center border border-transparent py-2 lg:hidden text-base font-medium text-orange-600 shadow-sm bg-orange-100"><ArrowBackIosNewRounded /></Link>
+            <AnimationLayout>
+                <ServicesItemPage />
+            </AnimationLayout>
+        </div>
     )
 }
