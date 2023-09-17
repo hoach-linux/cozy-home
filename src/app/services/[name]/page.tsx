39141fe76f63df86dd73a4fb9e2af93b3ebbdc81
hoach-linux <hoachprogrammer@gmail.com>
2023-09-17 11:07:18 +0700

    redesign butotn

--- src/app/services/[name]/page.tsx
+++ src/app/services/[name]/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 
     return (
         <div>
-            <Link href={"/"} className="z-10 fixed top-0 left-0 min-w-full hover:p-3 flex items-center justify-center border border-transparent py-2 lg:hidden text-base font-medium text-orange-600 shadow-sm bg-orange-100"><ArrowBackIosNewRounded /></Link>
+            <Link href={"/"} className="z-10 fixed top-0 left-0 min-w-full hover:p-4 flex items-center justify-center border border-transparent py-3 lg:hidden text-base font-medium text-orange-600 shadow-sm bg-orange-100"><ArrowBackIosNewRounded /></Link>
             <AnimationLayout>
                 <ServicesItemPage />
             </AnimationLayout>
