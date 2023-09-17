a95667a8a0e2cc645ad50e0ddad89781790f2c2a
hoach-linux <hoachprogrammer@gmail.com>
2023-09-17 11:48:59 +0700

    added loading page for finish page

---
+++ src/app/finish/loading.tsx
@@ -0,0 +1,10 @@
+import { Backdrop, CircularProgress } from "@mui/material";
+
+
+export default function Loading() {
+    return (
+        <Backdrop open>
+            <CircularProgress color="inherit" />
+        </Backdrop>
+    )
+}
